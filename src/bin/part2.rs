745e0f34d82a3511a23bb2133b137997eb8b56d3
Dalia Rojas Vallejo <daliarojasvallejo@proton.me>
2023-12-01 09:47:07 -0500

    Create part 2 template

---
+++ src/bin/part2.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let input = include_str!("../{{project-name}}_input.txt");
+    let result = process_input(input);
+    println!("{result}");
+}
+
+pub fn process_input(input: &str) {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_2() {
+        let input = "";
+
+        let result = process_input(input);
+
+        assert_eq!(, result);
+    }
+}
+
